af9e7eb45b1a940330fddf0286771076fbb323ce
nnnkit <iankit17@gmail.com>
2019-09-10 20:24:40 +0530

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,41 @@
 require(`dotenv`).config({
   path: `.env`,
-})
+});
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Emilia - Gatsby Starter Portfolio`,
+    // Used for the title template on pages other than the index site
+    siteTitle: `Lyrical Shades`,
+    // Default title of the page
+    siteTitleAlt: `Lyrical Shades`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `Lyrical Shades`,
+    // Will be used to generate absolute URLs for og:image etc.
+    siteUrl: `https://lyricalshades.com`,
+    // Used for SEO
+    siteDescription: `A wesite maintained by Preeti`,
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Twitter Handle
+    author: `@preetikumari06`,
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-emilia`,
-      options: {},
+      options: {
+        socialMedia: [
+          { title: `Twitter`, href: `https://twitter.com/preetikumari06` },
+          {
+            title: `Instagram`,
+            href: `https://www.instagram.com/lyrical_shades/`,
+          },
+        ],
+        showThemeAuthor: false,
+        name: 'Lyrical Shades',
+        location: 'Delhi',
+      },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
@@ -21,9 +47,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Emilia - @lekoarts/gatsby-theme-emilia`,
-        short_name: `Emilia`,
-        description: `Minimalistic portfolio/photography site with masonry grid, page transitions and big images. Themeable with Theme UI.`,
+        name: `Lyrical Shades`,
+        short_name: `Lyrical Shades`,
+        description: `A place to find words and picture with emotion.`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#3182ce`,
@@ -45,4 +71,4 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
   ],
-}
+};
